e2c5d8c8d0ba3ddd2f61192d393237f1a9fce769
Felipe <ukfelipeferreira@gmail.com>
2025-08-15 13:49:04 -0300

    atualizacao8

--- build-info.js
+++ build-info.js
@@ -1,11 +1,14 @@
-const fs = require("fs");
-const path = require("path");
+import { writeFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const buildInfo = {
   date: new Date().toISOString(),
 };
 
-fs.writeFileSync(
-  path.join(__dirname, "public", "build.json"),
+writeFileSync(
+  join(__dirname, "public", "build.json"),
   JSON.stringify(buildInfo, null, 2)
 );
